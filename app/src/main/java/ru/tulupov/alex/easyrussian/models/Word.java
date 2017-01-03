fb186400e8b4881abf530c1c47fea4fec9c324b8
Algresh <tulupov.alex@gmail.com>
2017-01-04 00:59:47 +0300

    u

---
+++ app/src/main/java/ru/tulupov/alex/easyrussian/models/Word.java
@@ -0,0 +1,37 @@
+package ru.tulupov.alex.easyrussian.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+
+public class Word extends RealmObject {
+
+    @PrimaryKey
+    protected int id;
+    protected String russianWord;
+    protected String englishWord;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRussianWord() {
+        return russianWord;
+    }
+
+    public void setRussianWord(String russianWord) {
+        this.russianWord = russianWord;
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public void setEnglishWord(String englishWord) {
+        this.englishWord = englishWord;
+    }
+}
